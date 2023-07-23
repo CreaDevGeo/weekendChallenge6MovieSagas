617cc37f3ecbca5ba149118092e2be6570944318
Georgio Harris <123996128+CreaDevGeo@users.noreply.github.com>
2023-07-23 16:01:31 -0500

    Currently styling web page. Created a styling for nav bar. May add functionality to make nav bar stick on scroll.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,26 +11,37 @@ function App() {
   // - RENDERING -
   return (
     <Router>
-    <div className="App">
-      <h1>The Movies Saga!</h1>
-      <nav>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-      </nav>
-      
-        <Route path="/" exact>
-          <MovieList />
-        </Route>
+      <div className="App">
+        <header>
+          <h1>
+            <Link to="/">MOVIE<br/>SAGA</Link>
+          </h1>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/favorites">Favorites</Link>
+              </li>
+              <li>
+                <Link to="/">Genres</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
 
-        {/* Details page */}
-        <Route path="/details">
-          <Details />
-        </Route>
-
-        {/* Add Movie page */}
-    </div>
-      </Router>
+        <main>
+          <Route path="/" exact>
+            <MovieList />
+          </Route>
+  
+          {/* Details page */}
+          <Route path="/details">
+            <Details />
+          </Route>
+  
+          {/* Add Movie page */}
+        </main>
+        </div>
+    </Router>
   );
 }
 
